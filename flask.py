68a6993298ea57ec36eea498e10bfdd0149f7aba
cozy_hn <jhk2721@gmail.com>
2024-02-21 08:06:12 +0900

    Add:flask function

---
+++ flask.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import subprocess
+import shlex
+
+app = Flask(__name__)
+
+@app.route('/tts')
+def run_tts():
+    tts = request.args.get('tts', default='')
+    ttsid = request.args.get('ttsid', default='')
+    uid = request.args.get('uid', default='')
+    vid = request.args.get('vid', default='')
+    pitch = request.args.get('pitch', default='')
+
+    script_path = '../tts_all.py'
+
+    command = f'python {script_path} --tts "{tts}" --ttsid {ttsid} --uid {uid} --vid {vid} --pitch {pitch}'
+    args = shlex.split(command)
+
+    try:
+        result = subprocess.run(args, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        response = {
+            'status': 'success',
+            'output': result.stdout
+        }
+    except subprocess.CalledProcessError as e:
+        response = {
+            'status': 'error',
+            'error': e.stderr
+        }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
