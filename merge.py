8c5f866d8a615ed17d66bb03f62f10999d38f899
cozy-hn <jhk2721@gmail.com>
2024-02-22 02:14:22 +0900

    Update : fix copy part

--- merge.py
+++ merge.py
@@ -2,20 +2,21 @@ from RVC.infer.lib.train.process_ckpt import merge
 import argparse
 from firebase_util import initialize_firebase, download_model_if_not_exists, upload_file_to_firebase
 import os
+import shutil
 
 # def merge(path1, path2, alpha1, sr, f0, info, path3, version):
 def start_merge():
-    defalut_path = "/home/jhko2721/vridge/Database/"
+    default_path = "/home/jhko2721/vridge/Database/"
     path1 = f"{args.uid1}/{args.vid1}/{args.vid1}.pth"
     path2 = f"{args.uid1}/{args.vid2}/{args.vid2}.pth"
     path3 = f"{args.uid1}/{args.vid3}/{args.vid3}.pth"
     index3 = f"{args.uid1}/{args.vid3}/{args.vid3}.index"
-    download_model_if_not_exists(f"{defalut_path}{path1}", f"{defalut_path}{path1}.index", args.uid1, args.vid1)
-    download_model_if_not_exists(f"{defalut_path}{path2}", f"{defalut_path}{path2}.index", args.uid1, args.vid2)
-    merge(f"{defalut_path}{path1}", f"{defalut_path}{path2}", args.alpha1, args.sr, args.f0, args.info, f"{defalut_path}{path3}", args.version)
-    os.copy(f"{defalut_path}{path1[:-4]}.index", f"{defalut_path}{index3}")
-    upload_file_to_firebase(f"{defalut_path}{path3}", path3)
-    upload_file_to_firebase(f"{defalut_path}{index3}", index3)
+    download_model_if_not_exists(f"{default_path}{path1}", f"{default_path}{path1}.index", args.uid1, args.vid1)
+    download_model_if_not_exists(f"{default_path}{path2}", f"{default_path}{path2}.index", args.uid1, args.vid2)
+    merge(f"{default_path}{path1}", f"{default_path}{path2}", args.alpha1, args.sr, args.f0, args.info, f"{default_path}{path3}", args.version)
+    shutil.copy(f"{default_path}{path1[:-4]}.index", f"{default_path}{index3}")
+    upload_file_to_firebase(f"{default_path}{path3}", path3)
+    upload_file_to_firebase(f"{default_path}{index3}", index3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Merge two models')
