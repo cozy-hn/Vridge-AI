258433648893be7b76576e4e6f86e2c1a4ba65d5
cozy-hn <jhk2721@gmail.com>
2024-02-22 02:09:16 +0900

    Update : fix args

--- merge.py
+++ merge.py
@@ -13,7 +13,7 @@ def start_merge():
     download_model_if_not_exists(f"{defalut_path}{path1}", f"{defalut_path}{path1}.index", args.uid1, args.vid1)
     download_model_if_not_exists(f"{defalut_path}{path2}", f"{defalut_path}{path2}.index", args.uid1, args.vid2)
     download_model_if_not_exists(f"{defalut_path}{path3}", f"{defalut_path}{path3}.index", args.uid1, args.vid3)
-    merge(f"{defalut_path}{path1}", f"{defalut_path}{path2}", 0.5, "48K", 1, "", f"{defalut_path}{path3}", "v2")
+    merge(f"{defalut_path}{path1}", f"{defalut_path}{path2}", args.alpha1, args.sr, args.f0, args.info, f"{defalut_path}{path3}", args.version)
     os.copy(f"{defalut_path}{path1[:-4]}.index", f"{defalut_path}{index3}")
     upload_file_to_firebase(f"{defalut_path}{path3}", path3)
     upload_file_to_firebase(f"{defalut_path}{index3}", index3)
@@ -24,11 +24,11 @@ if __name__ == '__main__':
     parser.add_argument('vid1', type=str, help='VID1')
     parser.add_argument('vid2', type=str, help='VID2')
     parser.add_argument('vid3', type=str, help='VID3')
-    parser.add_argument('alpha1', type=float, help='Alpha1', default=0.5) #알파 0.5로 고정
-    parser.add_argument('sr', type=int, help='Sample rate', default="48K") #샘플링 레이트 "48K"로 고정
-    parser.add_argument('f0', type=int, help='F0', default=1) #F0 1로 고정
-    parser.add_argument('info', type=str, help='Info', default="")
-    parser.add_argument('version', type=int, help='Version', default="v2") #모델 버전
+    parser.add_argument('--alpha1', type=float, help='Alpha1', default=0.5) #알파 0.5로 고정
+    parser.add_argument('--sr', type=int, help='Sample rate', default="48K") #샘플링 레이트 "48K"로 고정
+    parser.add_argument('--f0', type=int, help='F0', default=1) #F0 1로 고정
+    parser.add_argument('--info', type=str, help='Info', default="")
+    parser.add_argument('--version', type=int, help='Version', default="v2") #모델 버전
     args = parser.parse_args()
     initialize_firebase()
     start_merge()
